156115ea282180c339fe3e1cf5e06d6bdc052da2
Ricardo Pupo Larguesa <rlarguesa@gmail.com>
2024-01-30 08:57:34 -0300

    Importação do projeto

---
+++ src/java/api/TaskServlet.java
@@ -0,0 +1,145 @@
+package api;
+
+import jakarta.servlet.ServletConfig;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import model.Task;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author rlarg
+ */
+@WebServlet(name = "TaskServlet", urlPatterns = {"/tasks"})
+public class TaskServlet extends HttpServlet {
+    
+    private JSONObject getJSONBody(BufferedReader reader) throws IOException{
+        StringBuilder buffer = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        return new JSONObject(buffer.toString());
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        JSONObject file = new JSONObject();
+        try {
+            file.put("exception", Task.exception);
+            ArrayList<Task> list = Task.getList();
+            JSONArray arr = new JSONArray();
+            for(Task t: list){
+                JSONObject o = new JSONObject();
+                o.put("title", t.getTitle());
+                arr.put(o);
+            }
+            file.put("list", arr);
+            response.getWriter().print(file.toString());
+        }catch(Exception ex){
+            file.put("error", ex.getLocalizedMessage());
+            response.getWriter().print(file.toString());
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        JSONObject file = new JSONObject();
+        try {
+            //Reading body
+            JSONObject body = getJSONBody(request.getReader());
+            //Adding task, if exists
+            String title = body.getString("title");
+            if(title != null){
+                Task.addTask(title);
+            }
+            //Returning tasks
+            ArrayList<Task> list = Task.getList();
+            JSONArray arr = new JSONArray();
+            for(Task t: list){
+                JSONObject o = new JSONObject();
+                o.put("title", t.getTitle());
+                arr.put(o);
+            }
+            file.put("list", arr);
+            response.getWriter().print(file.toString());
+        }catch(Exception ex){
+            file.put("error", ex.getLocalizedMessage());
+            response.getWriter().print(file.toString());
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>DELETE</code> method.
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        JSONObject file = new JSONObject();
+        try {
+            String title = request.getParameter("title");
+            Task.removeTask(title);
+            ArrayList<Task> list = Task.getList();
+            JSONArray arr = new JSONArray();
+            for(Task t: list){
+                JSONObject o = new JSONObject();
+                o.put("title", t.getTitle());
+                arr.put(o);
+            }
+            file.put("list", arr);
+            response.getWriter().print(file.toString());
+        }catch(Exception ex){
+            response.setStatus(500); //Internal error
+            file.put("error", ex.getLocalizedMessage());
+            response.getWriter().print(file.toString());
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        Task.createTable();
+    }
+
+}
---
+++ src/java/model/Task.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.util.ArrayList;
+import java.sql.*;
+
+public class Task {
+    public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:to-do.db";
+    
+    public static Exception exception = null;
+    
+    public static void createTable(){
+        try{
+            Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            stmt.execute("create table if not exists tasks(title varchar not null)");
+            stmt.close();
+            con.close();
+        }catch(Exception ex){
+            exception = ex;
+        }
+    }
+    
+    public static Connection getConnection() throws Exception{
+        Class.forName(CLASS_NAME);
+        return DriverManager.getConnection(URL);
+    }
+    
+    public static ArrayList<Task> getList() throws Exception{
+        ArrayList<Task> list = new ArrayList<>();
+        Connection con = getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from tasks");
+        while(rs.next()){
+            list.add(new Task(rs.getString("title")));
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return list;
+    }
+    
+    public static void addTask(String title) throws Exception{
+        Connection con = getConnection();
+        PreparedStatement stmt = con.prepareStatement("insert into tasks values(?)");
+        stmt.setString(1, title);
+        stmt.execute();
+        stmt.close();
+        con.close();
+    }
+    
+    public static void removeTask(String title) throws Exception{
+        Connection con = getConnection();
+        PreparedStatement stmt = con.prepareStatement("delete from tasks where title = ?");
+        stmt.setString(1, title);
+        stmt.execute();
+        stmt.close();
+        con.close();
+    }
+    
+    public static ArrayList<Task> list = new ArrayList<>();
+    
+    private String title;
+
+    public Task() {
+        this.setTitle("[NEW]");
+    }
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
